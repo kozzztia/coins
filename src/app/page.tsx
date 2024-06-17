16ccc5a92162aee91d655952945a6ebdcb04498f
kostia <kozzztia@ukr.net>
2024-06-17 23:58:01 +0300

    api with useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import Image from "next/image";
+'use client'
 import styles from "css/page.module.css";
 import { Coin } from "types/coins";
+import { useEffect, useState } from "react";
 
-export default async function Home() {
-  const coins = await fetch('http://localhost:3000/api/coins').then(res => res.json());
+export default function Home() {
+
+  const [coins, setCoins] = useState<Coin[]>([]);
+
+
+  useEffect(() => {
+   fetch('http://localhost:3000/api/coins')
+   .then(res => res.json().then(data=> setCoins(data.coins)))
+  }, []);
 
   return (
     <main className={styles.main}>
-      {coins.coins.map((coin: Coin) => (
+      {coins?.map((coin: Coin) => (
         <div key={coin.id}>
           <h1>{coin.name}</h1>
           <p>{coin.symbol}</p>
